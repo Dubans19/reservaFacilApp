4ea8060a1f2425878b90bac973157573dae8e015
Duban <duvanandres2005@gmail.com>
2024-08-22 11:45:55 -0500

    nuevos cambios

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use("/",authRouter)
 app.get("/traer",(req,res)=>{
-    res.json("traer")
+    res.json("endpoint prueba")
 })
 const PORT=3000
 app.listen(PORT,()=>console.log("Servidor en el puerto "  +  PORT))
